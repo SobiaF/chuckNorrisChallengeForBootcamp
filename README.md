17930f469ce7c9289df68947042b7919572ad7ca
Sobia Furqan <sobiafurqan@hotmail.com>
2021-07-19 23:27:31 +0100

    formatted files. Updated README.md with challenge as well

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# chuckNorrisChallengeAtBootcamp
+# Chuck Norris Challenge at Bootcamp 
+[fetch] Create a random chuck norris joke app!
+Sending a GET request to https://api.chucknorris.io/jokes/random will
+result in a random chuck norris joke being sent to you!
+Display the random joke inside of an H1 tag
+Add a button that allows you to fetch a brand new random joke
--- app.js
+++ app.js
@@ -1,26 +1,15 @@
-// [fetch] Create a random chuck norris joke app!
-// Sending a GET request to https://api.chucknorris.io/jokes/random will
-// result in a random chuck norris joke being sent to you!
-// Display the random joke inside of an H1 tag
-// Add a button that allows you to fetch a brand new random joke
-
-
-function jokeApp () {
+function jokeApp() {
     fetch('https://api.chucknorris.io/jokes/random', {
         method: 'GET',
         headers: {
             'content-type': 'application/json'
         }
-    }).then(function(response) {
+    }).then(function (response) {
         return response.json();
-    }).then(function(data) {
-        console.log(data.value); // this line
+    }).then(function (data) {
         let jokes = document.querySelector('#jokes');
-        console.log(jokes);
         jokes.innerHTML = data.value;
     })
 }
 
-
 document.querySelector("#clickForJoke").addEventListener("click", jokeApp);
-
